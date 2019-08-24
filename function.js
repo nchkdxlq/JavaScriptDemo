c9648ea48e4a8e43154708bd14d4b65fb44d08fe
luoquan <nchkdxlq.gmail.com>
2019-08-24 17:00:35 +0800

    高阶函数

--- function.js
+++ function.js
@@ -178,3 +178,196 @@ myAge; // 20 ，把执行的属性赋值给指定的变量
 var {name, single = true} = person;
 name;
 single;
+
+
+
+
+
+
+// 高阶函数：函数的参数为函数时，称为高阶函数
+
+console.log('========== 高阶函数 ========');
+
+var intArr = [1, 2, 3, 4];
+
+function pow(x) {
+    return x * x;
+}
+
+// pow作为函数的参数传递给 map
+var res = intArr.map(pow);
+console.log('map: ' + res);
+
+// reduce   参数的函数foo(x, y)接收两个参数, x为当前结果, y为序列的下一个元素
+
+// 利用reduxe求一个数组的和
+function sum(x, y) {
+    return x + y;
+}
+var res = intArr.reduce(sum);
+console.log('reduce 求和: ' + res);
+
+
+function string2Int(s) {
+    var arr = [];
+    for (var i = 0; i < s.length; i++) {
+        arr[i] = s[i];
+    }
+
+    function char2Int(x) {
+        return x - '0';
+    }
+    function arr2Number(x, y) {
+        return 10 * x + y;
+    }
+    
+    return arr.map(char2Int).reduce(arr2Number);
+    return arr.map(function(x){return x - '0';}).reduce(function(x, y){return 10 * x + y;});
+}
+
+var res = string2Int('123');
+console.log('string2Int: ' + res);
+
+
+// filter 过滤一些元素，返回满足条件的元素
+
+var intArr = [1, 2, 3, 4, 5, 6];
+var res = intArr.filter(function (x) { return x % 2});
+console.log('filter: ' + res); // 1, 3, 5
+
+
+// sort() 默认情况先把元素转换为string在排序，总结：默认根据ASIIC码字符序
+
+['Google', 'apple', 'Microsoft'].sort(); // ['Apple', 'Google', 'Microsoft'];
+
+['Google', 'apple', 'Microsoft'].sort(); // ['Google', 'Microsoft", 'apple']
+
+
+var intArr = [10, 12, 3, 14, 25, 6];
+
+intArr.sort(); //10,12,14,25,3,6
+
+var res = intArr.sort(function (x, y) {
+    return x - y; // 返回-1，表示x在y的前面，即 x-y升序， y-x降序
+});
+// 3,6,10,12,14,25
+
+
+var arr = ['Google', 'apple', 'Microsoft'];
+
+// 忽略大小写比较
+
+var res = arr.sort(function(x, y) {
+    x = x.toUpperCase(); y = y.toUpperCase();
+    // x = x.toLowerCase(); y = y.toLowerCase();
+
+    if (x < y) {
+        return -1;
+    } else {
+        return 1;
+    }
+});
+
+// ["apple", "Google", "Microsoft"]
+
+
+
+
+// Arrow Function (ES6标准新增了一种新的函数: 箭头函数)
+
+// 普通函数
+var f1 = function(x) {
+    return x * x;
+}
+// 箭头函数
+var f2 = x => x * x;
+f1(2);
+f2(2);
+
+// 多个参数，需要用()括起来
+(x, y) => x + y;
+
+// 无参数
+() => 2004;
+
+// 可变参数
+(x, y, ...rest) => {
+    var i, sum = x + y;
+    for (i = 0; i < rest.length; i++) {
+        sum += rest[i];
+    }
+    return sum;
+};
+
+// 还缺少箭头函数对 `this` 指针的讨论， 待补充
+
+
+
+
+
+// generator 生成器  是ES6标准引入的新的数据类型。一个generator看上去像一个函数，但可以返回多次。
+
+
+// 编写一个产生斐波那契数列的函数
+function fib(max) {
+    var
+        t,
+        a = 0,
+        b = 1,
+        arr = [0, 1];
+    while (arr.length < max) {
+        [a, b] = [b, a + b];
+        arr.push(b);
+    }
+    return arr;
+}
+
+// 测试:
+fib(5); // [0, 1, 1, 2, 3]
+fib(10); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+
+function* fib(max) {
+    var 
+        t,
+        a = 0,
+        b = 1,
+        n = 0;
+    while(n < max) {
+        yield a;
+        [a, b] = [b, a + b];
+        n++;
+    }
+
+    return;
+}
+
+
+var f = fib(5); // 创建一个生成器对象, 还没有执行
+var {value, done} = f.next(); // {value: 0, done: false}
+console.log('value = ' + value, 'done = ' + done);
+
+f.next(); // {value: 1, done: false}
+f.next(); // {value: 1, done: false}
+f.next(); // {value: 2, done: false}
+f.next(); // {value: 3, done: false}
+f.next(); // {value: undefined, done: true}
+
+// next()方法会执行generator的代码，然后，每次遇到yield x;就会返回一个对象{value: x, done: false/true}, 然后暂停，
+// value就是yield的返回值，done表示这个generator是否执行结束，如果done为true，则value就是return的返回值
+
+for (var x of fib(10)) {
+    console.log(x); // 依次输出0, 1, 1, 2, 3, ...
+}
+
+
+// 实现一个自增id的生成器
+
+function* increaseMent() {
+    var num = 0;
+    while (true) {
+        yield num;
+        num++;
+    }
+}
+
